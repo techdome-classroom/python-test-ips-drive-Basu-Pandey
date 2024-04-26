94fade5a72a221314baabda0112ac2cbd27b750a
Basu Pandey <basupandey1234@gmail.com>
2024-04-26 13:17:17 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -8,6 +8,25 @@ def longest_substring(s: str) -> int:
 
     """ 
     pass
+n = len(s)
+    if n == 0:
+        return 0
 
+    max_length = 0
+    start = 0
+    char_index_map = {}
+
+    for end in range(n):
+        if s[end] in char_index_map:
+            
+            start = max(start, char_index_map[s[end]] + 1)
+        
+       
+        char_index_map[s[end]] = end
+        
+        
+        max_length = max(max_length, end - start + 1)
+    
+    return max_lengt
 
 
