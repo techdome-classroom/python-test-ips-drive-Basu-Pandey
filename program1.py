e198bf97cfc34c49da4e8cf4d8f15fb27ed8b084
Basu Pandey <basupandey1234@gmail.com>
2024-04-26 13:15:30 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -7,7 +7,24 @@ def smallest_missing_positive_integer(nums: List[int]) -> int:
     """
     pass
 
+from typing import List
 
+def smallest_missing_positive_integer(nums: List[int]) -> int:
+
+    
+    nums = set(filter(lambda x: x > 0, nums))
+
+    
+    if not nums:
+        return 1
+
+    
+    max_num = max(nums)
+
+
+    for i in range(1, max_num + 2):
+        if i not in nums:
+            return i
 
 
 
